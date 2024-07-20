6d0af41523249d76b7943f105e1335e39898d6ce
user <funnymoments610@gmail.com>
2024-07-20 23:47:11 +0200

    Now project has a burger menu. I created this at first time!

--- mobileapp/scripts/burger.js
+++ mobileapp/scripts/burger.js
@@ -1,10 +1,6 @@
 document.getElementById("burgerButtonOpen").onclick = openBurger;
 document.getElementById("burgerButtonClose").onclick = closeBurger;
 
-document.getElementById("modalButtonOpen").onclick = openModal; //
-document.getElementById("modalButtonClose").onclick = closeModal;
-
-let modal = document.querySelector(".modal");
 let burger = document.querySelector(".burger");
 let all = document.querySelector("body");
 
@@ -19,14 +15,3 @@ function closeBurger() {
     burger.style.display = "none";
     all.style.overflow = "scroll";
 }
-
-function openModal() {
-    modal.style.display = "block";
-    all.style.overflow = "hidden";
-}
-
-function closeModal() {
-    modal.style.display = "none";
-}
-
-//console.log(all)
