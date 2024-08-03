53339d70fe64b97095746651a85c64c4ac554018
user <funnymoments610@gmail.com>
2024-08-03 21:23:32 +0200

    v3.0. completed

--- california/scripts/showBurger.js
+++ california/scripts/showBurger.js
@@ -2,13 +2,16 @@ document.getElementById("burgerButtonOpen").onclick = openBurger;
 document.getElementById("burgerButtonClose").onclick = closeBurger;
 
 let burger = document.getElementById("burgerModalWindow");
+let all = document.querySelector("body");
 
 //console.log(modal.style.display); --> на память)
 
 function openBurger() {
     burger.style.display = "block";
+    all.style.overflow = "hidden";
 }
 
 function closeBurger() {
     burger.style.display = "none";
+    all.style.overflow = "scroll";
 }
