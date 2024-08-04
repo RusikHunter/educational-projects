7a521185263f2594f6d573a33956b5462932640c
user <funnymoments610@gmail.com>
2024-08-04 21:19:07 +0200

    volumenzeit v.1.2 header adaptive

--- volumenzeit/scripts/showModalWindowHeader.js
+++ volumenzeit/scripts/showModalWindowHeader.js
@@ -1,9 +1,13 @@
+/* скрипты, показывающие модальные окна в header */
+
+/* показать/скрыть окно регистрации и логина */
+
 let headerProfileBtn = document.getElementById("headerProfileButton");
 let headerModalWindow = document.querySelector(".header__modal-window");
 
 let isOpen = false;
 
-headerProfileBtn.onclick = (e) => {
+headerProfileBtn.onclick = () => {
     if (isOpen == false) {
         headerModalWindow.style.display = "block";
         isOpen = true;
@@ -12,4 +16,23 @@ headerProfileBtn.onclick = (e) => {
         headerModalWindow.style.display = "none";
         isOpen = false;
     }
+}
+
+/* показать/скрыть бургер меню */
+
+let burgerButtonOpen = document.getElementById("burgerButtonOpen");
+let burgerButtonClose = document.getElementById("burgerButtonClose");
+
+let burger = document.querySelector(".header__burger");
+
+let all = document.querySelector("body");
+
+burgerButtonOpen.onclick = () => {
+    burger.style.display = "flex";
+    all.style.overflow = "hidden";
+}
+
+burgerButtonClose.onclick = () => {
+    burger.style.display = "none";
+    all.style.overflow = "scroll";
 }
