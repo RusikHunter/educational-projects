44db7723d309f543e9c0d6a895077bf4d74cc908
user <funnymoments610@gmail.com>
2024-08-29 18:46:16 +0200

    volumenzeit v12.1.1

--- volumenzeit/scripts/dropdownCountry.js
+++ volumenzeit/scripts/dropdownCountry.js
@@ -3,6 +3,11 @@ let decorationC = document.querySelector(".country__decoration")
 let contentC = document.querySelector(".country__form")
 let isOpenC = false;
 
+let dropdownC1 = document.querySelector(".country__wrap--second");
+let decorationC1 = document.querySelector(".country__decoration--second")
+let contentC1 = document.querySelector(".country__form--second")
+let isOpenC1 = false;
+
 dropdownC.onclick = () => {
     if (isOpenC === false) {
         decorationC.style.display = "block";
@@ -15,3 +20,20 @@ dropdownC.onclick = () => {
         isOpenC = false;
     }
 }
+
+dropdownC1.onclick = () => {
+    if (isOpenC1 === false) {
+        decorationC1.style.display = "block";
+        contentC1.style.display = "flex";
+        isOpenC1 = true;
+    }
+    else {
+        decorationC1.style.display = "none";
+        contentC1.style.display = "none";
+        isOpenC1 = false;
+    }
+}
+
+document.querySelector(".login__form-singup").addEventListener("submit", function (event) {
+    event.preventDefault();
+});
