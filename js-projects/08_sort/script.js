605bc1d8ffda73e95b1bf18a951f0f82dd8a4925
dmfshove <funnymoments610@gmail.com>
2024-10-01 12:17:59 +0200

    js 08_sort v2

--- js-projects/08_sort/script.js
+++ js-projects/08_sort/script.js
@@ -1,21 +1,24 @@
-const array = [23, 1, 9, 87, 16, 32, 8, 17, 17, 8]
+const array = [1, 2, 21, 15, 5, 6, 8, 17, 16, 20]
 
 function sort(arr) {
     const arrToSorting = [...arr] // [1, 23, 9, 87, 16, 32, 8, 17, 17, 8]
     let elementsToCheck = arrToSorting.length - 1 // 9
+    let swapped = false
 
     for (let i = 0; i < arrToSorting.length - 2; ++i) { // 8
+        swapped = false
+
         for (let y = 0; y < elementsToCheck; ++y) {
             if (arrToSorting[y] > arrToSorting[y + 1]) {
-                let temp = arrToSorting[y]
+                [arrToSorting[y], arrToSorting[y + 1]] = [arrToSorting[y + 1], arrToSorting[y]]
 
-                arrToSorting[y] = arrToSorting[y + 1]
-
-                arrToSorting[y + 1] = temp
+                swapped = true
             }
         }
 
         elementsToCheck--
+
+        if (!swapped) break
     }
 
     return arrToSorting
