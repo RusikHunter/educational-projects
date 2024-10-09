fb749320402ba32323f786c1c40d150e56a25b30
dmfshove <funnymoments610@gmail.com>
2024-10-09 18:40:48 +0200

    timer v8

--- projects/01_timer/scripts/Window.js
+++ projects/01_timer/scripts/Window.js
@@ -2,24 +2,33 @@ import Timer from './Timer.js'
 import Stopwatch from './Stopwatch.js'
 
 class Window {
+    isFirstWindow = true
+
     constructor(timer, stopwatch) {
-        this.timer = document.querySelector('.timer')
-        this.stopwatch = document.querySelector('.stopwatch')
+        this.timer = timer
+        this.stopwatch = stopwatch
+
+        this.timerElement = document.querySelector('.timer')
+        this.stopwatchElement = document.querySelector('.stopwatch')
 
         this.toggleButtonElement = document.querySelector('.window__toggle-button')
 
         this.toggleButtonElement.addEventListener('click', () => {
-            this.timer.classList.toggle('unvisible')
-            this.stopwatch.classList.toggle('unvisible')
+            this.timerElement.classList.toggle('unvisible')
+            this.stopwatchElement.classList.toggle('unvisible')
+
+            if (this.isFirstWindow) {
+                this.timer.stopTimer()
+            } else {
+                this.stopwatch.stopStopwatch()
+            }
+
+            this.isFirstWindow = !this.isFirstWindow
         })
-    }
-
-    toggle() {
-
     }
 }
 
 const timer = new Timer()
 const stopwatch = new Stopwatch()
 
-const window = new Window()
+const window = new Window(timer, stopwatch)
