622a8222155f1cef45c6c7556e6e46e9fc6f25f7
dmfshove <funnymoments610@gmail.com>
2024-10-11 19:30:49 +0200

    calculator v2

--- projects/02_calculator/scripts/App.js
+++ projects/02_calculator/scripts/App.js
@@ -1,6 +1,13 @@
 class Calculator {
+    firstNumber = ""
+    secondNumber = ""
+    operator = ""
+
     constructor() {
         this.#getElements()
+        this.#addEvents()
+
+        console.log(this.operationButtons)
     }
 
     #getElements() {
@@ -36,7 +43,139 @@ class Calculator {
     }
 
     #addEvents() {
+        this.numberButtons.forEach((button, index) => {
+            button.addEventListener('click', () => {
+                if (this.operator === "") {
+                    this.firstNumber += index
+                } else if (this.operator !== "") {
+                    this.secondNumber += index
+                }
 
+                this.#showNumber()
+            })
+        });
+
+        this.operationButtons.forEach((button, index) => {
+            button.addEventListener('click', () => {
+                if (this.firstNumber === "") return
+
+                let operation
+
+                switch (index) {
+                    case 0:
+                        operation = "+"
+                        break
+                    case 1:
+                        operation = "-"
+                        break
+                    case 2:
+                        operation = "*"
+                        break
+                    case 3:
+                        operation = "/"
+                        break
+                }
+
+                this.operator = operation
+
+                this.#showNumber()
+            })
+        })
+
+        this.changeButtons.forEach((button, index) => {
+            button.addEventListener('click', () => {
+                if (this.firstNumber === "") return
+
+                let operation
+
+                if (index === 0) {
+                    if (this.firstNumber !== "" && this.secondNumber === "") {
+                        this.firstNumber = String(Math.pow(this.firstNumber, 2))
+                    } else {
+                        this.secondNumber = String(Math.pow(this.secondNumber, 2))
+                    }
+                } else if (index === 1) {
+                    if (this.firstNumber !== "" && this.secondNumber === "") {
+                        this.firstNumber = String(Math.sqrt(Math.abs(this.firstNumber), 2))
+                    } else {
+                        this.secondNumber = String(Math.sqrt(Math.abs(this.secondNumber), 2))
+                    }
+                } else if (index === 2) {
+                    if (this.firstNumber !== "" && this.secondNumber === "") {
+                        if (this.firstNumber.startsWith('-')) {
+                            this.firstNumber = this.firstNumber.slice(1)
+                        } else {
+                            this.firstNumber = `-${this.firstNumber}`
+                        }
+                    } else {
+                        if (this.secondNumber.startsWith('-')) {
+                            this.secondNumber = this.secondNumber.slice(1)
+                        } else {
+                            this.secondNumber = `-${this.secondNumber}`
+                        }
+                    }
+                } else {
+                    if (this.firstNumber !== "" && this.secondNumber === "") {
+                        const floatSymbolIndex = this.firstNumber.indexOf('.')
+
+                        console.log(floatSymbolIndex)
+
+                        if (floatSymbolIndex === -1) {
+                            console.log('a')
+                            this.firstNumber += '.'
+                        } else {
+                            console.log('b')
+                            this.firstNumber = this.firstNumber.slice(0, floatSymbolIndex)
+                        }
+                    } else {
+                        const floatSymbolIndex = this.secondNumber.indexOf('.')
+
+                        console.log(floatSymbolIndex)
+
+                        if (floatSymbolIndex === -1) {
+                            console.log('a')
+                            this.secondNumber += '.'
+                        } else {
+                            console.log('b')
+                            this.secondNumber = this.secondNumber.slice(0, floatSymbolIndex)
+                        }
+                    }
+                }
+
+                this.#showNumber()
+            })
+        })
+
+        this.buttonRemoveElement.addEventListener('click', () => {
+            if (this.operator === "") {
+                this.firstNumber = this.firstNumber.slice(0, -1)
+            } else if (this.operator !== "" && this.secondNumber === "") {
+                this.operator = ""
+            } else {
+                this.secondNumber = this.secondNumber.slice(0, -1)
+            }
+
+            this.#showNumber()
+        })
+
+        this.buttonRemoveAllElement.addEventListener('click', () => {
+            this.outputElement.textContent = ""
+            this.firstNumber = ""
+            this.secondNumber = ""
+            this.operator = ""
+        })
+    }
+
+    #showNumber() {
+        this.outputElement.textContent = this.firstNumber
+
+        if (this.operator !== "") {
+            this.outputElement.textContent += this.operator
+        }
+
+        if (this.secondNumber !== "") {
+            this.outputElement.textContent += this.secondNumber
+        }
     }
 }
 
