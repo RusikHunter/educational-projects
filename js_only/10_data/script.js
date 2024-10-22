210a65933686a8480824a63fc6eda07b7a2b5b3e
dmfshove <funnymoments610@gmail.com>
2024-10-22 19:40:18 +0200

    chat v1

---
+++ js_only/10_data/script.js
@@ -0,0 +1,7 @@
+// document.cookie = "username=JohnDoe";
+// console.log(document.cookie);
+
+// localStorage.setItem('user', JSON.stringify({ name: 'John', age: 30 }));
+// console.log(localStorage)
+
+console.log(window.history)
