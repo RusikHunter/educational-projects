35f6153296851fb66b7c30a03687bdd0ce73c71d
dmfshove <funnymoments610@gmail.com>
2024-10-31 10:09:19 +0100

    09_shop v7

--- projects/09_shop/src/scripts/components/ShoppingListHandler.js
+++ projects/09_shop/src/scripts/components/ShoppingListHandler.js
@@ -29,6 +29,15 @@ class ShoppingListHandler {
         // select sort type
 
         this.elements.sortingTypeSelectElement = document.querySelector('[data-js-select-sort-type]')
+
+        // edit product form
+
+        this.elements.editProductDialogElement = document.querySelector('[data-js-dialog-edit-product]')
+        this.elements.editProductFormElement = document.querySelector('[data-js-form-edit-product]')
+        this.elements.editProductFormCloseButtonElement = document.querySelector('[data-js-button-change-close]')
+        this.elements.editProductFormIDElement = document.querySelector('[data-js-form-edit-id]')
+        this.elements.editProductFormTitleElement = document.querySelector('[data-js-form-edit-title]')
+        this.elements.editProductFormSubmitButtonElement = document.querySelector('[data-js-form-edit-submit]')
     }
 
     #_setEvents() {
@@ -51,6 +60,38 @@ class ShoppingListHandler {
             const sortingType = event.target.value
 
             this.sortingType = sortingType
+
+            this.#_renderProductsList()
+        })
+
+        this.elements.editProductFormCloseButtonElement.addEventListener('click', () => {
+            this.elements.editProductDialogElement.removeAttribute('open')
+        })
+
+        this.elements.editProductFormSubmitButtonElement.addEventListener('click', (event) => {
+            event.preventDefault()
+
+            const formData = new FormData(this.elements.editProductFormElement)
+
+            const product = JSON.parse(window.localStorage.getItem(this.elements.editProductDialogElement.getAttribute('data-js-dialog-edit-product')))
+
+            const productProperties = []
+
+            for (const [key, value] of formData.entries()) {
+                if (product.hasOwnProperty(key)) {
+                    if (value.trim() !== '') {
+                        productProperties.push(value)
+                    } else {
+                        productProperties.push(product[key])
+                    }
+                }
+            }
+
+            const newProduct = new Product(product.id, productProperties[0], productProperties[1], productProperties[2], productProperties[3], productProperties[4])
+
+            window.localStorage.setItem(product.id, JSON.stringify(newProduct))
+
+            this.elements.editProductDialogElement.removeAttribute('open')
 
             this.#_renderProductsList()
         })
@@ -98,6 +139,7 @@ class ShoppingListHandler {
 
         switch (this.sortingType) {
             case 'nothing':
+                console.log('nothing')
                 const keys = Object.keys(window.localStorage).sort()
 
                 for (const key of keys) {
@@ -171,7 +213,25 @@ class ShoppingListHandler {
             })
         })
 
-        //  todo todo todo todo todo => => =>
+        // add event listeners to all rendered edit-buttons
+
+        const editButton = document.querySelectorAll('[data-js-button-edit]')
+        editButton.forEach(button => {
+            button.addEventListener('click', () => {
+                const id = button.getAttribute('data-js-button-edit')
+
+                this.elements.editProductDialogElement.setAttribute('open', '')
+
+                const product = JSON.parse(window.localStorage.getItem(id))
+
+                this.elements.editProductFormIDElement.textContent = `ID: ${product.id}`
+                this.elements.editProductDialogElement.setAttribute('data-js-dialog-edit-product', product.id)
+                this.elements.editProductFormTitleElement.textContent = product.title
+
+                // this.#_renderProductsList()
+            })
+        })
+
         // adding event listeners to all rendered remove-buttons
         const checkboxes = document.querySelectorAll('[data-js-checkbox-is-purchased]')
         checkboxes.forEach(checkbox => {
