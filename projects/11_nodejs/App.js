927e2066c85446fe59edee173fcfc52ace61098b
dmfshove <funnymoments610@gmail.com>
2024-11-10 20:16:40 +0100

    nodejs v3

--- projects/11_nodejs/App.js
+++ projects/11_nodejs/App.js
@@ -5,3 +5,15 @@ const _ = require('lodash')
 
 const arr = [1, 2, 3, 4]
 console.log(_.shuffle(arr))
+
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/plain')
+    res.end('Hello, Node.js!')
+})
+
+server.listen(3000, () => {
+    console.log('Сервер запущен на http://localhost:3000')
+});
