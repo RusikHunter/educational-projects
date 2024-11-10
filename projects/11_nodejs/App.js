18aaffaacfc3d8133e79d060177c1bbac31ee52a
dmfshove <funnymoments610@gmail.com>
2024-11-10 20:14:52 +0100

    nodejs v2

--- projects/11_nodejs/App.js
+++ projects/11_nodejs/App.js
@@ -1,2 +1,7 @@
-const greet = require('./greet');
-greet("World");
+const greet = require('./greet')
+greet("World")
+
+const _ = require('lodash')
+
+const arr = [1, 2, 3, 4]
+console.log(_.shuffle(arr))
--- projects/11_nodejs/greet.js
+++ projects/11_nodejs/greet.js
@@ -1,3 +1,3 @@
 module.exports = function (name) {
-    console.log(`Hello, ${name}!`);
+    console.log(`Hello, ${name}!`)
 };
