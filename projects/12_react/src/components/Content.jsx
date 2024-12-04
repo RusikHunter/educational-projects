24cbbad0e42cf009558ba021c9e94a348688fce5
dmfshove <funnymoments610@gmail.com>
2024-12-04 12:03:01 +0100

    react v6

--- projects/12_react/src/components/Content.jsx
+++ projects/12_react/src/components/Content.jsx
@@ -1,7 +1,19 @@
-import logo from '../icons/logo.png'
+import logo from '../assets/logo.png'
+import data from '../assets/data.json'
 
 const logoPath = logo
 const altText = 'Illusion'
+
+function ListItem({ title, description }) {
+    return (
+        <>
+            <li className="content__list-item">
+                <h6 className="content__title">{title}</h6>
+                <p className="content__description">{description}</p>
+            </li>
+        </>
+    )
+}
 
 export default function Content() {
     return (
@@ -12,7 +24,13 @@ export default function Content() {
                         <div className="content__column">
                             <p className="content__time">ID: {Math.floor(Math.random() * 100)}</p>
 
-                            <img src={logoPath} alt={altText} />
+                            <img className="content__image" src={logoPath} alt={altText} />
+
+                            <ul className="content__list">
+                                <ListItem title={data[0].title} description={data[0].description} />
+                                <ListItem title={data[1].title} description={data[1].description} />
+                                <ListItem title={data[2].title} description={data[2].description} />
+                            </ul>
                         </div>
                     </div>
                 </div>
