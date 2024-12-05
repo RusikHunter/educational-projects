3a16220d283f7341c87a2e803ceada8ed016b846
dmfshove <funnymoments610@gmail.com>
2024-12-05 11:46:46 +0100

    react v9

--- projects/12_react/src/components/Button/Button.jsx
+++ projects/12_react/src/components/Button/Button.jsx
@@ -1,8 +1,12 @@
 import './Button.css'
 
 export default function Button(props) {
+    function logContent() {
+        console.log('Some content.')
+    }
+
     return (
-        <button className="button">
+        <button className="button" onClick={logContent}>
             {props.text}
         </button>
     )
