9953e6391098c98038619ee13abfef11743b23df
dmfshove <funnymoments610@gmail.com>
2024-12-08 15:15:02 +0100

    tab v6

--- projects/13_interactive_tab/src/components/SectionFeedback/SectionFeedback.jsx
+++ projects/13_interactive_tab/src/components/SectionFeedback/SectionFeedback.jsx
@@ -10,6 +10,7 @@ const FormStyledComponent = styled.form`
 export default function SectionFeedback() {
     const [name, setName] = useState('')
     const [reason, setReason] = useState('help')
+    const [isError, setIsError] = useState(false)
 
     return (
         <section className="feedback">
@@ -17,7 +18,11 @@ export default function SectionFeedback() {
 
             <FormStyledComponent>
                 <label htmlFor="name">Your name:</label>
-                <input type="text" id="name" value={name} onChange={event => setName(event.target.value)} />
+                <input type="text" id="name" value={name} onChange={event => {
+                    setName(event.target.value)
+                    setIsError(event.target.value.trim().length === 0)
+                }}
+                    style={{ border: isError ? "3px solid red" : null }} />
 
                 <label htmlFor="reason">Your feedback:</label>
                 <select id="reason" value={reason} onChange={event => setReason(event.target.value)}>
