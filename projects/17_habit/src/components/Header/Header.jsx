7f0df88a390db206e2a62b385894de210dfeb337
dmfshove <funnymoments610@gmail.com>
2024-12-20 12:02:31 +0100

    habit v4

--- projects/17_habit/src/components/Header/Header.jsx
+++ projects/17_habit/src/components/Header/Header.jsx
@@ -10,33 +10,52 @@ const LogoLetterStyledElement = styled.span`
     color: #406BD9;
 `
 
+const NavigationStyledElement = styled.nav`
+    width: 100%;
+`
+
+const UnorderedListStyledElement = styled.ul`
+display: flex; /* Устанавливаем flexbox для горизонтального размещения */
+justify-content: space-between; /* Распределяем элементы с равными отступами */
+padding: 0; /* Убираем отступы по умолчанию у списка */
+margin: 0; /* Убираем внешние отступы */
+list-style: none; /* Убираем маркеры списка */
+width: 100%; /* Растягиваем список на всю ширину родителя */
+gap: 25px;
+`
+
+const ListItemStyledElement = styled.li`
+flex: 1; /* Каждый элемент занимает равное пространство */
+text-align: center; /* Центрируем текст в каждом элементе */
+`
+
 export default function Header() {
     return (
         <>
             <header className="header">
                 <div className="header__container container">
                     <div className="header__row row">
-                        <div className="header__column header__column--first">
+                        <div className="header__column header__column--first column">
                             <a href="#">
                                 <LogoTextStyledElement>habit<LogoLetterStyledElement className="blue">s</LogoLetterStyledElement></LogoTextStyledElement>
                             </a>
                         </div>
-                        <div className="header__column header__column--second">
-                            <nav>
-                                <ul>
-                                    <li>
+                        <div className="header__column header__column--second column">
+                            <NavigationStyledElement>
+                                <UnorderedListStyledElement>
+                                    <ListItemStyledElement>
                                         <Link to="/">Main</Link>
-                                    </li>
-                                    <li>
+                                    </ListItemStyledElement>
+                                    <ListItemStyledElement>
                                         <Link to="/habit">Habit</Link>
-                                    </li>
-                                    <li>
+                                    </ListItemStyledElement>
+                                    <ListItemStyledElement>
                                         <Link to="/progress">Progress</Link>
-                                    </li>
-                                </ul>
-                            </nav>
+                                    </ListItemStyledElement>
+                                </UnorderedListStyledElement>
+                            </NavigationStyledElement>
                         </div>
-                        <div className="header__column header__column--third"></div>
+                        <div className="header__column header__column--third column"></div>
                     </div>
                 </div>
             </header>
