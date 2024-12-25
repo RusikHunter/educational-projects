838435bcb40d3edda32c61407c2154b3bad5b82f
dmfshove <funnymoments610@gmail.com>
2024-12-25 20:05:50 +0100

    list v3

--- projects/17_list/src/components/ProductList.jsx
+++ projects/17_list/src/components/ProductList.jsx
@@ -1,24 +1,31 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import ProductItem from "./ProductItem"
 
 export default function ProductList() {
-    const [products, setProducts] = useState([
-        {
-            name: "default product"
-        },
-        {
-            name: "product n123"
-        },
-        {
-            name: "apple"
+    const [products, setProducts] = useState([])
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        const fetchProducts = async () => {
+            try {
+                const response = await fetch('/products.json')
+                const data = await response.json()
+                setProducts(data)
+            } catch (error) {
+                console.error("Ошибка загрузки данных:", error)
+            } finally {
+                setLoading(false)
+            }
         }
-    ])
+
+        fetchProducts()
+    }, [])
 
     return (
         <div>
             <ul>
-                {products.map(product => (
-                    <li>
+                {products.map((product, index) => (
+                    <li key={index}>
                         <ProductItem name={product.name} />
                     </li>
                 ))}
