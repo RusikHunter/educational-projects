cdec9d00a89eb4d253ee5378daf8e42d6461f99f
dmfshove <funnymoments610@gmail.com>
2024-12-26 14:17:43 +0100

    redux v1

---
+++ projects/18_redux/src/App.jsx
@@ -0,0 +1,29 @@
+import { useDispatch, useSelector } from 'react-redux';
+
+function App() {
+    const dispatch = useDispatch()
+    const cash = useSelector(state => state.cash)
+
+    const addCash = () => {
+        dispatch({ type: "ADD_CASH", payload: 5 })
+    }
+
+    const getCash = () => {
+        dispatch({ type: "GET_CASH", payload: 5 })
+    }
+
+    return (
+        <>
+            <h1>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Libero nihil iure, impedit magni, quod rem numquam modi asperiores laboriosam quasi blanditiis consequatur alias, iste temporibus! Debitis maiores dolorem quibusdam non.
+            </h1>
+
+            <p>{cash}</p>
+
+            <button onClick={addCash}>Add money</button>
+            <button onClick={getCash}>Get money</button>
+        </>
+    )
+}
+
+export default App
