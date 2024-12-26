5ebbe160a0a34ab8a8091e246c39c3651df4c40f
dmfshove <funnymoments610@gmail.com>
2024-12-26 14:54:12 +0100

    redux v3

--- projects/18_redux/src/store/index.js
+++ projects/18_redux/src/store/index.js
@@ -1,12 +1,13 @@
 import { createStore, combineReducers } from 'redux'
 import cashReducer from './cashReducer.js'
 import customerReducer from './customerReducer.js'
+import { composeWithDevTools } from "redux-devtools-extension"
 
 const rootReducer = combineReducers({
     cash: cashReducer,
     customer: customerReducer
 })
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer, composeWithDevTools())
 
 export default store
