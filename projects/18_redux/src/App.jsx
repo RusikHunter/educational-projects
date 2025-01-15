8122ff7d47323562cd2ad7ddf1853419f9c83701
nikita <funnymoments610@gmail.com>
2025-01-15 18:42:32 +0100

    redux v4

--- projects/18_redux/src/App.jsx
+++ projects/18_redux/src/App.jsx
@@ -3,6 +3,8 @@ import { useDispatch, useSelector } from 'react-redux';
 function App() {
     const dispatch = useDispatch()
     const cash = useSelector(state => state.cash.cash)
+    const customers = useSelector(state => state.customers.customers)
+
 
     const addCash = () => {
         dispatch({ type: "ADD_CASH", payload: 5 })
@@ -10,6 +12,14 @@ function App() {
 
     const getCash = () => {
         dispatch({ type: "GET_CASH", payload: 5 })
+    }
+
+    const addCustomer = () => {
+        dispatch({ type: "ADD_CUSTOMER", payload: "Alex" })
+    }
+
+    const getCustomer = () => {
+        dispatch({ type: "GET_CUSTOMER", payload: "Alex" })
     }
 
     return (
@@ -20,8 +30,20 @@ function App() {
 
             <p>{cash}</p>
 
+            {customers.length > 0 ?
+                <p>
+                    {customers.map(customer =>
+                        <div>{customer}</div>
+                    )}
+                </p>
+                :
+                <p>No clients</p>
+            }
+
             <button onClick={addCash}>Add money</button>
             <button onClick={getCash}>Get money</button>
+            <button onClick={addCustomer}>Add customer</button>
+            <button onClick={getCustomer}>Get customer</button>
         </>
     )
 }
--- projects/18_redux/src/store/customerReducer.js
+++ projects/18_redux/src/store/customerReducer.js
@@ -1,13 +1,13 @@
 const defaultState = {
-    customer: []
+    customers: []
 }
 
 const customerReducer = (state = defaultState, action) => {
     switch (action.type) {
-        case "ADD_CASH":
-            return { ...state, cash: state.cash + action.payload }
-        case "GET_CASH":
-            return { ...state, cash: state.cash - action.payload }
+        case "ADD_CUSTOMER":
+            return { ...state, customers: [...state.customers, action.payload] }
+        case "GET_CUSTOMER":
+            return { ...state, customers: state.customers.slice(0, -1) }
         default:
             return state
     }
