7f45d8fbb260c5ced30e025b27164b4d88538c42
nikita <funnymoments610@gmail.com>
2025-01-16 18:30:06 +0100

    toolkit v1

---
+++ projects/20_toolkit/src/App.jsx
@@ -0,0 +1,16 @@
+import { increment, decrement } from "./toolkitRedux/toolkitReducer"
+import { useDispatch, useSelector } from 'react-redux'
+
+export default function App() {
+    const dispatch = useDispatch()
+    const count = useSelector(state => state.toolkit.count)
+
+    return (
+        <>
+            <h1>counter: {count}</h1>
+
+            <button onClick={() => dispatch(increment())}>+</button>
+            <button onClick={() => dispatch(decrement())}>-</button>
+        </>
+    )
+}
---
+++ projects/20_toolkit/src/toolkitRedux/toolkitReducer.js
@@ -0,0 +1,22 @@
+import { createReducer, createAction } from '@reduxjs/toolkit'
+
+const initialState = {
+    count: 0,
+    todos: ['eating', 'programming', 'الله أكبر ديناميت متفجرات بلاستيكية بطيخ بطيخ يوسفي'],
+}
+
+export const increment = createAction('INCREMENT')
+export const decrement = createAction('DECREMENT')
+
+
+export default createReducer(
+    initialState, (builder) => {
+        builder
+            .addCase(increment, (state) => {
+                state.count += 1
+            })
+            .addCase(decrement, (state) => {
+                state.count -= 1
+            })
+    }
+)
