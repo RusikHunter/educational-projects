b3947b0bb0069fceb58b715af267e4d17f558fc3
dmfshove <funnymoments610@gmail.com>
2025-04-07 19:41:22 +0200

    jest unit v3

--- projects/28_jest_unit/src/mapArrToString/mapArrToString.test.js
+++ projects/28_jest_unit/src/mapArrToString/mapArrToString.test.js
@@ -4,4 +4,13 @@ describe('mappArrToString', () => {
     test('Корректное значение', () => {
         expect(mapArrToString([1, 2, 3])).toEqual(['1', '2', '3'])
     })
+    test('Мешанина', () => {
+        expect(mapArrToString([1, 2, 3, null, undefined, 'ssfsdf'])).toEqual(['1', '2', '3'])
+    })
+    test('Пустой массив', () => {
+        expect(mapArrToString([])).toEqual([])
+    })
+    test('Отрицание', () => {
+        expect(mapArrToString([1, 2, 3])).not.toEqual([1, 2, 3, 4])
+    })
 })
