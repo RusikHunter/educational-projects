4c34fa511287ca2e92deadfdf9fcfcbe25409978
dmfshove <funnymoments610@gmail.com>
2025-04-08 20:04:29 +0200

    react testing library v7

--- projects/29_react_testing_library/src/users/Users.jsx
+++ projects/29_react_testing_library/src/users/Users.jsx
@@ -15,8 +15,8 @@ export default function Users() {
     }, [])
 
     return (
-        <>
+        <div data-testid="users-page">
             {users.map(user => <Link to={`/users/${user.id}`} key={user.id} data-testid="user-item">{user.name}</Link>)}
-        </>
+        </div>
     )
 }
