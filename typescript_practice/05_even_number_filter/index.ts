fe979a3b6c143c59509aebf3d996fefa58801972
dmfshove <funnymoments610@gmail.com>
2025-09-04 11:21:23 +0200

    typescript practice 05 typification

--- typescript_practice/05_even_number_filter/index.ts
+++ typescript_practice/05_even_number_filter/index.ts
@@ -1,8 +1,8 @@
-function evenNumberFilter(array) {
-    const arrayWithEvenNumbers = array.filter(item => item % 2 === 0)
+function evenNumberFilter(array: number[]): number[] {
+    const arrayWithEvenNumbers: number[] = array.filter(item => item % 2 === 0)
 
     return arrayWithEvenNumbers
 }
 
-const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const array: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 console.log(evenNumberFilter(array))
