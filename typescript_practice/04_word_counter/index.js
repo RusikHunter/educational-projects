49354e40572b1f13090bc71e9fe62b197fca42b6
dmfshove <funnymoments610@gmail.com>
2025-09-04 11:15:51 +0200

    typescript practice 04 optimization

--- typescript_practice/04_word_counter/index.js
+++ typescript_practice/04_word_counter/index.js
@@ -5,37 +5,29 @@
 //         return acc
 //     }, {})
 //     const keysArray: string[] = Object.keys(wordsObject)
-
 //     const WORDS_ARRAY_LENGTH: number = wordsArray.length
 //     const KEYS_ARRAY_LENGTH: number = keysArray.length
-
 //     for (let i = 0; i < KEYS_ARRAY_LENGTH; ++i) {
 //         for (let j = 0; j < WORDS_ARRAY_LENGTH; ++j) {
-
 //             if (wordsArray[j] === keysArray[i]) {
 //                 wordsObject[keysArray[i]]++
 //             }
 //         }
 //     }
-
 //     return wordsObject
 // }
-
-function countWords(string: string): Record<string, number> {
-    const wordsArray: string[] = string.split(" ").map(word => word.toLowerCase()) // Apple, banana, apple, string, fdff, gthgf, out, banana, banAnA
-
-    const wordsObject: Record<string, number> = wordsArray.reduce((accumulator, word) => {
+function countWords(string) {
+    var wordsArray = string.split(" ").map(function (word) { return word.toLowerCase(); }); // Apple, banana, apple, string, fdff, gthgf, out, banana, banAnA
+    var wordsObject = wordsArray.reduce(function (accumulator, word) {
         if (word in accumulator) {
-            accumulator[word]++
-        } else {
-            accumulator[word] = 1
+            accumulator[word]++;
         }
-
-        return accumulator
-    }, {})
-
-    return wordsObject
+        else {
+            accumulator[word] = 1;
+        }
+        return accumulator;
+    }, {});
+    return wordsObject;
 }
-
-const string = "Apple banana apple string fdff gthgf out banana banAnA"
-console.log(countWords(string))
+var string = "Apple banana apple string fdff gthgf out banana banAnA";
+console.log(countWords(string));
