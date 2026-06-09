9e12ab775c45c5f76fd7aa7e18af72397670e190
dmfshove <funnymoments610@gmail.com>
2026-06-09 21:15:51 +0200

    time formatter v1

--- typescript_practice/08_time_formatter/index.ts
+++ typescript_practice/08_time_formatter/index.ts
@@ -1,6 +1,21 @@
-type FormattedTimeObject = {
+type FormattedTimeObjectType = {
     days: number,
     hours: number,
     minutes: number,
     seconds: number
+}
+
+const SECONDS_OF_DAY = 86400
+const SECONDS_OF_HOUR = 3600
+const SECONDS_OF_MINUTE = 60
+
+const formatTime = function (seconds: number): FormattedTimeObjectType {
+    const countOfSeconds = seconds
+
+    const formattedTimeObject = {}
+
+    formattedTimeObject.days = 
+
+
+    return "abc"
 }
