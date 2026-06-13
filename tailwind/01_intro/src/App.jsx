caa1ddcaf8e317ffa2511a78e1cf5db5d36285b2
dmfshove <funnymoments610@gmail.com>
2026-06-13 13:19:56 +0200

    tailwind 01_intro v6 main

--- tailwind/01_intro/src/App.jsx
+++ tailwind/01_intro/src/App.jsx
@@ -18,6 +18,26 @@ function App() {
             <main>
                 Main
                 <h1 className="text-7xl text-green-500 font-medium">Hello World!</h1>
+
+                <div className="grid grid-cols-3 gap-6 mt10">
+                    <div className="">image</div>
+                    <div className="">image</div>
+                    <div className="">image</div>
+                    <div className="">image</div>
+                    <div className="">image</div>
+                    <div className="">image</div>
+                    <div className="">image</div>
+                    <div className="">image</div>
+                    <div className="">image</div>
+                    <div className="">image</div>
+                    <div className="">image</div>
+                    <div className="">image</div>
+                    <div className="">image</div>
+                    <div className="">image</div>
+                    <div className="">image</div>
+                </div>
+
+                <button className="flex justify-center items-center mt-50 mx-auto w-100 h-30 bg-gray-400 rounded-4xl hover:bg-gray-700 transition-colors duration-300 ease-in-out cursor-pointer animate-bounce shadow-xl shadow-green-100">Add Image</button>
             </main>
 
             <footer className="mt-10 bg-emerald-100 border-4 rounded-lg">
