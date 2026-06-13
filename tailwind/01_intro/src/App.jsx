70b401b7d60439e16562c03b303ab90b462d8555
dmfshove <funnymoments610@gmail.com>
2026-06-13 12:47:20 +0200

    tailwind 01_intro v4

--- tailwind/01_intro/src/App.jsx
+++ tailwind/01_intro/src/App.jsx
@@ -1,7 +1,14 @@
 function App() {
     return (
         <>
-            <h1 className="text-7xl text-green-500 font-medium">Hello World!</h1>
+            <header className="bg-emerald-100 border-2">Header</header>
+            <main>
+                Main
+                <h1 className="text-7xl text-green-500 font-medium">Hello World!</h1>
+            </main>
+            <footer className="mt-10 bg-emerald-100 border-4 rounded-lg">
+                Footer
+            </footer>
         </>
     )
 }
