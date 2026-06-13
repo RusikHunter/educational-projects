e3d51ab6af46d15862e190195f3511dac0bf551e
dmfshove <funnymoments610@gmail.com>
2026-06-13 12:55:12 +0200

    tailwind 01_intro v5 header

--- tailwind/01_intro/src/App.jsx
+++ tailwind/01_intro/src/App.jsx
@@ -1,11 +1,25 @@
 function App() {
     return (
         <>
-            <header className="bg-emerald-100 border-2">Header</header>
+            <header className="bg-emerald-100 border-2 flex items-center justify-between w-96 mx-auto">
+                <div className="">Logo</div>
+
+                <ul>
+                    <li>gfgd</li>
+                    <li>fhff</li>
+                    <li>fgdfh</li>
+                    <li>hgfhf</li>
+                    <li>fhgh</li>
+                </ul>
+
+                <button>Contat us</button>
+            </header>
+
             <main>
                 Main
                 <h1 className="text-7xl text-green-500 font-medium">Hello World!</h1>
             </main>
+
             <footer className="mt-10 bg-emerald-100 border-4 rounded-lg">
                 Footer
             </footer>
