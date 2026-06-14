553b63356afa94abeb34c1ee4fdff34a1aa26acb
dmfshove <funnymoments610@gmail.com>
2026-06-14 13:32:18 +0200

    eslint and prettier bind

--- tailwind_and_linter/01_intro/eslint.config.js
+++ tailwind_and_linter/01_intro/eslint.config.js
@@ -1,32 +1,39 @@
-import js from "@eslint/js";
-import globals from "globals";
-import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
+import js from '@eslint/js';
+import globals from 'globals';
+import pluginReact from 'eslint-plugin-react';
+import { defineConfig } from 'eslint/config';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default defineConfig([
   js.configs.recommended,
   pluginReact.configs.flat.recommended,
 
   {
-    files: ["**/*.{js,mjs,cjs,jsx}"],
-
+    files: ['**/*.{js,mjs,cjs,jsx}'],
+    plugins: {
+      js,
+      prettier: eslintPluginPrettier,
+    },
+    extends: ['js/recommended', eslintConfigPrettier],
     languageOptions: {
       globals: globals.browser,
     },
 
     settings: {
       react: {
-        version: "detect",
+        version: 'detect',
       },
     },
 
     rules: {
-      "react/react-in-jsx-scope": "off",
-      "react/jsx-uses-react": "off",
-      "no-console": "warn",
-      eqeqeq: "warn",
-      curly: "warn",
-      "no-else-return": "warn",
+      ...eslintPluginPrettier.configs.recommended.rules,
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-uses-react': 'off',
+      'no-console': 'warn',
+      eqeqeq: 'warn',
+      curly: 'warn',
+      'no-else-return': 'warn',
     },
   },
 ]);
