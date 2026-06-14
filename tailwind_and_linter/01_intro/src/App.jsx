6afe309934549a97eb29c31df0081e5efb6c506b
dmfshove <funnymoments610@gmail.com>
2026-06-14 13:16:21 +0200

    prettier v1

--- tailwind_and_linter/01_intro/src/App.jsx
+++ tailwind_and_linter/01_intro/src/App.jsx
@@ -1,55 +1,58 @@
 function App() {
+  return (
+    <>
+      <header className="bg-emerald-100 border-2 flex items-center justify-between w-96 mx-auto">
+        <div className="">Logo</div>
 
-    return (
-        <>
-            <header className="bg-emerald-100 border-2 flex items-center justify-between w-96 mx-auto">
-                <div className="">Logo</div>
+        <ul>
+          <li>gfgd</li>
+          <li>fhff</li>
+          <li>fgdfh</li>
+          <li>hgfhf</li>
+          <li>fhgh</li>
+        </ul>
 
-                <ul>
-                    <li>gfgd</li>
-                    <li>fhff</li>
-                    <li>fgdfh</li>
-                    <li>hgfhf</li>
-                    <li>fhgh</li>
-                </ul>
+        <button>Contat us</button>
+      </header>
 
-                <button>Contat us</button>
-            </header>
+      <main>
+        Main
+        <h1 className="text-7xl text-green-500 font-medium">Hello World!</h1>
+        <div className="grid grid-cols-3 gap-6 mt10">
+          <div className="">image</div>
+          <div className="">image</div>
+          <div className="">image</div>
+          <div className="">image</div>
+          <div className="">image</div>
+          <div className="">image</div>
+          <div className="">image</div>
+          <div className="">image</div>
+          <div className="">image</div>
+          <div className="">image</div>
+          <div className="">image</div>
+          <div className="">image</div>
+          <div className="">image</div>
+          <div className="">image</div>
+          <div className="">image</div>
+        </div>
+        <button className="flex justify-center items-center mt-50 mx-auto w-100 h-30 bg-gray-400 rounded-4xl hover:bg-gray-700 transition-colors duration-300 ease-in-out cursor-pointer animate-bounce shadow-xl shadow-green-100">
+          Add Image
+        </button>
+      </main>
 
-            <main>
-                Main
-                <h1 className="text-7xl text-green-500 font-medium">Hello World!</h1>
+      <footer className="mt-10 bg-emerald-100 border-4 rounded-lg">
+        <form>
+          <input
+            className="px-2 py-1 rounded border border-solid border-transparent duration-500 ease-linear transition-colors hover:border-green-500 focus:border-purple-500 outline-none shadow-none block mb-10"
+            type="text"
+            placeholder="Enter email:"
+          />
 
-                <div className="grid grid-cols-3 gap-6 mt10">
-                    <div className="">image</div>
-                    <div className="">image</div>
-                    <div className="">image</div>
-                    <div className="">image</div>
-                    <div className="">image</div>
-                    <div className="">image</div>
-                    <div className="">image</div>
-                    <div className="">image</div>
-                    <div className="">image</div>
-                    <div className="">image</div>
-                    <div className="">image</div>
-                    <div className="">image</div>
-                    <div className="">image</div>
-                    <div className="">image</div>
-                    <div className="">image</div>
-                </div>
-
-                <button className="flex justify-center items-center mt-50 mx-auto w-100 h-30 bg-gray-400 rounded-4xl hover:bg-gray-700 transition-colors duration-300 ease-in-out cursor-pointer animate-bounce shadow-xl shadow-green-100">Add Image</button>
-            </main>
-
-            <footer className="mt-10 bg-emerald-100 border-4 rounded-lg">
-                <form>
-                    <input className="px-2 py-1 rounded border border-solid border-transparent duration-500 ease-linear transition-colors hover:border-green-500 focus:border-purple-500 outline-none shadow-none block mb-10" type="text" placeholder="Enter email:" />
-
-                    <button className="inline-block sm:inline-block"></button>
-                </form>
-            </footer>
-        </>
-    )
+          <button className="inline-block sm:inline-block"></button>
+        </form>
+      </footer>
+    </>
+  );
 }
 
-export default App
+export default App;
--- tailwind_and_linter/01_intro/src/main.jsx
+++ tailwind_and_linter/01_intro/src/main.jsx
@@ -1,15 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-const a = 3
-const b = 4
+const a = 3;
+const b = 4;
 
-console.log(a == b)
+console.log(a == b);
 
-createRoot(document.getElementById('root')).render(
-    <StrictMode>
-        <App />
-    </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
